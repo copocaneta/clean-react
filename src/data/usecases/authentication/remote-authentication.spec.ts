884fc61d02ad1842130dea44f65019562430f29d
Thiago Bernardi <copocaneta@gmail.com>
2022-02-26 17:35:57 -0300

    refactor: renaming httpPostClient to httpPostClientSpy on test

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,20 +1,20 @@
-import { HttpPostClient } from 'data/protocols/http/http-post-client';
-import { RemoteAuthentication } from './remote-authentication';
+import { HttpPostClient } from 'data/protocols/http/http-post-client'
+import { RemoteAuthentication } from './remote-authentication'
 
 describe('RemoteAuthentication', () => {
   test('Should call http client with correct URL', async () => {
-    class HttpClientSpy implements HttpPostClient {
-      url?: string;
+    class HttpPostClient implements HttpPostClient {
+      url?: string
       async post(url: string): Promise<void> {
-        this.url = url;
-        return Promise.resolve();
+        this.url = url
+        return Promise.resolve()
       }
     }
-    const url = 'any_url';
-    const httPostClientSpy = new HttpClientSpy();
+    const url = 'any_url'
+    const httPostClientSpy = new HttpPostClient()
     // sut = system under test
-    const sut = new RemoteAuthentication(url, httPostClientSpy);
-    await sut.auth();
-    expect(httPostClientSpy.url).toBe(url);
-  });
-});
+    const sut = new RemoteAuthentication(url, httPostClientSpy)
+    await sut.auth()
+    expect(httPostClientSpy.url).toBe(url)
+  })
+})
