153309b411052191b98bc7245d6497913d259a0a
Thiago Bernardi <copocaneta@gmail.com>
2022-03-01 23:13:20 -0300

    feat: ensure login present spinner on submit

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -8,7 +8,7 @@ type Props = React.HTMLAttributes<HTMLElement>
 
 const Spinner: React.FC<Props> = (props: Props): JSX.Element => {
   return (
-    <div {... props} className={[Styles.spinner, props.className].join(' ')}>
+    <div {... props} data-testid="spinner" className={[Styles.spinner, props.className].join(' ')}>
         <div/>
         <div/>
         <div/>
